eef392be5b6c35b98bf11a51de1b0d228b30c305
Sumio K <tap.miwo@gmail.com>
2022-07-14 06:59:57 +0900

    rebuild addmin

---
+++ app/models/book.rb
@@ -0,0 +1,8 @@
+class Book < ApplicationRecord
+    scope :costly,->{ where("price > ?", 3000)}
+    scope :written_about,->(theme){where("name like ?","%#{theme}%")}
+
+    belongs_to :publisher
+    has_many :book_authors
+    has_many :authors,through::book_authors
+end
