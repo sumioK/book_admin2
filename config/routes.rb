1c4fd586ebf89085b0ba23e15b1113da75eadc1f
Sumio K <tap.miwo@gmail.com>
2022-07-16 11:36:03 +0900

    add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/books/:id" => "books#show"
+  delete "/books/:id" => "books#destroy"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resource :publishers
 end
