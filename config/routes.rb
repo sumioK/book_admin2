9381441bce4e2dc844c9fcdfc3c86714a0aa50b8
Sumio K <tap.miwo@gmail.com>
2022-07-17 22:20:18 +0900

    create index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
  
   get "/books/:id" => "books#show"
   delete "/books/:id" => "books#destroy"
+  get "/books" => "books#index"
 
   resources :publishers
   resource :profiel, only: %i{show edit update}
