a31d6f97d96db6c739826bb43cc9d0a69a9bdd34
Sumio K <tap.miwo@gmail.com>
2022-07-23 07:45:20 +0900

    edit root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
  
   get "/books/:id" => "books#show"
   delete "/books/:id" => "books#destroy"
-  get "/books" => "books#index"
+  get "/" => "books#index"
 
   resources :publishers
   resource :profiel, only: %i{show edit update}
